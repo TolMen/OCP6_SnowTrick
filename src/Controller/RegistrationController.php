aaf56df9df1e7f771c890c36676375322a41e49e
TolMen <cotton.frachisse@gmail.com>
2024-09-29 16:17:20 +0200

    Edit minor

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -53,7 +53,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('_profiler_home'); //Remplacer par la page d'accueil
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('registration/register.html.twig', [
