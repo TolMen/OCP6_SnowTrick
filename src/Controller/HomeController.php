fcac2979ea1f99589668ab67391c1bc51a01b395
TolMen <cotton.frachisse@gmail.com>
2024-10-01 19:26:49 +0200

    Link to retrieve trick information

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
+        // Récupération de tous les tricks avec leurs images
+        $tricks = $trickRepository->findAllWithImages();
+
         return $this->render('home/index.html.twig', [
+            'tricks' => $tricks,
             'controller_name' => 'HomeController',
         ]);
     }
