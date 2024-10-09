fc0daf52958a7e2005cb775a8905fd2765322ce1
TolMen <cotton.frachisse@gmail.com>
2024-10-09 14:21:58 +0200

    Adding UML

---
+++ UML/Modéle de données -- Trick.sql
@@ -0,0 +1,13 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `trick` (`id`, `name`, `content`, `category`, `date_created`, `date_updated`, `user_id`, `slug`) VALUES
+(1,	'Ollie',	'Un saut réalisé sans utiliser de kicker, en frappant le tail de la planche contre le sol pour prendre de la hauteur.',	'Sauts',	'2024-09-23',	NULL,	15,	'ollie'),
+(2,	'Nose Grab',	'Attraper le nez de la planche pendant un saut.',	'Grabs',	'2024-09-23',	NULL,	15,	'nose-grab'),
+(3,	'Tail Grab',	'Attraper le tail de la planche pendant un saut.',	'Grabs',	'2024-09-23',	NULL,	15,	'tail-grab'),
+(4,	'Frontside 360',	'Un saut où le snowboarder effectue une rotation complète de 360° dans le sens frontside.',	'Rotations',	'2024-09-23',	NULL,	15,	'frontside-360'),
+(5,	'Backside 360',	'Un saut avec une rotation de 360° dans le sens backside.',	'Rotations',	'2024-09-23',	NULL,	15,	'backside-360'),
+(6,	'Frontside 540',	'Un saut avec une rotation de 540°, avec l’épaule avant du snowboarder qui mène le mouvement.',	'Rotations',	'2024-09-23',	NULL,	15,	'frontside-540'),
+(7,	'Backflip',	'Un saut périlleux arrière où le snowboarder effectue une rotation en arrière.',	'Flips',	'2024-09-23',	NULL,	15,	'backflip'),
+(8,	'Method Grab',	'Attraper la carre arrière de la planche avec la main avant tout en fléchissant le corps vers le haut.',	'Grabs',	'2024-09-23',	NULL,	15,	'method-grab'),
+(9,	'Shifty',	'Pendant un saut, le snowboarder fait pivoter son corps et sa planche dans des directions opposées avant de ramener la planche dans sa position normale.',	'Tweaks',	'2024-09-23',	NULL,	15,	'shifty'),
+(10,	'Cab 720',	'Une rotation de 720° effectuée en switch (position inversée) dans le sens frontside.',	'Rotations',	'2024-09-23',	NULL,	15,	'cab-720');
